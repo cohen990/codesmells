d62e95657b7c09395a552a69dbd414e53578d2e3
Dan Cohen <dan.cohen@codurance.com>
2017-10-26 15:42:39 +0100

    add switch statements

--- codesmells.sh
+++ codesmells.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
+# a script that will use the command line arguments to navigate the directory structure and output the relevant information
 
-echo "this is a code smells program"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ "$1" = "" ] || [ "$1" = "--help" ]
+then
+  cat $DIR/.help
+  exit 0
+fi
+
+test -d $DIR/$1 && 
+	test -d $DIR/$1/$2 &&
+		cat $DIR/$1/$2/.help &&
+		exit 0
+	cat $DIR/$1/.help && 
+	exit 0
+exit 1
